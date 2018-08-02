a6a7e783afb05e6079cf609bc291668414077855
92hoy <ho_9209@naver.com>
2018-08-02 09:01:14 +0000

    protected grass

--- protect.sh
+++ protect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-date >> /home/ubuntu/project/protectgrass/history.log
+date >> /home/vagrant/project/protectgrass/history.log
 
-cd /home/ubuntu/project/protectgrass/
+cd /home/vagrant/project/protectgrass/
 
-/usr/bin/git add . >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git add .
 
-/usr/bin/git commit -m "protected grass" >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git commit -m "protected grass"
 
-/usr/bin/git push origin master >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git push origin master
 
